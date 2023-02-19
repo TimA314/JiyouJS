4499d121d9820a48e6a192778c62e6b3d98d6a8f
TimA314 <ci0d7j9m@mailer.me>
2023-02-18 21:41:05 -0800

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import {
+  generatePrivateKey,
+  relayInit, 
+  getPublicKey,
+  SimplePool,
+  validateEvent,
+  verifySignature,
+  signEvent,
+  getEventHash
+} from 'nostr-tools'
+import { useEffect, useState } from 'react';
 import './App.css';
+import ActionAreaCard from './components/card';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [events, setEvents ] = useState([]);
+  let relays = ['wss://nos.lol', 'wss://relay.damus.io', 'wss://nostr.mom']
+  // let privateKey = generatePrivateKey() // `sk` is a hex string
+  // // console.log("pk", privateKey)
+  // let publicKey = getPublicKey(privateKey) // `pk` is a hex string
+  // console.log("publicKey:", publicKey)
+
+  useEffect(()=> {
+    async function start(){
+      const pool = new SimplePool()
+
+
+      let events = await pool.list(relays, [{kinds: [1]}])
+      setEvents(events);
+      console.log(events);
+    }
+    start();
+    
+  },[]);
+  
+  if(events !== []){
+    return (
+      <div className="App">
+        {events.map((event) => (
+            <ActionAreaCard key={event.id} event={event} relays={relays}/>
+        ))}
+      </div>
+    )
+  }
+  else {
+    return <div></div>
+  }
 }
 
 export default App;
