f1e093a540cf4994b4146998719145b38f93156b
TimA314 <ci0d7j9m@mailer.me>
2023-03-03 21:54:18 -0800

    add profile

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import { ThemeProvider, createTheme, } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
+import { Container } from '@mui/system';
 
 import Feed from './pages/Feed';
 import LabelBottomNavigation from './components/NavBar';
@@ -9,6 +10,7 @@ import { Route, Routes } from 'react-router';
 import NewEventForm from './components/NewEventForm';
 import { generatePrivateKey, getPublicKey } from 'nostr-tools';
 import SignIn from './pages/SignIn';
+import Relays from './pages/Relays';
 
 export default function App() {
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
@@ -27,29 +29,25 @@ export default function App() {
 
   return (
       <ThemeProvider theme={theme}>
-        <Box
+        <Container
           sx={{
-            display: 'flex',
             width: '100%',
             alignItems: 'center',
             justifyContent: 'center',
             bgcolor: 'background.default',
             color: 'text.primary',
-            borderRadius: 1,
             p: 3,
-          }}
-        >
-          <Box>
+          }}>
             <LabelBottomNavigation />
             <Routes>
               <Route path="/" element={<Feed />} />
               <Route path="/new-event" element={<NewEventForm />} />
               <Route path="/profile" element={<SignIn />} />
               <Route path="/follows" element={<Feed />} />
+              <Route path="/relays" element={<Relays />} />
               <Route path="/signin" element={<SignIn />} />
             </Routes>
-          </Box>
-        </Box>
+        </Container>
       </ThemeProvider>
   );
 }
