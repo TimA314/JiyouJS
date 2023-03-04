e9e1da272898de216ea1d1d2ebd4b3272564bd03
TimA314 <ci0d7j9m@mailer.me>
2023-03-03 19:52:50 -0800

    display events

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useEffect } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
@@ -9,35 +10,32 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { useSelector } from 'react-redux';
 
 
-export default async function Note(props) {
-    let content = props.event.content;
-    let pubkey = props.event.pubkey;
-    let profile = await nip05.queryProfile(pubkey)
+export default async function Note() {
+    const globalEvents = useSelector(state => state.nostr.events);
 
-  return (
-    <div>
-        <Box sx={{ width: '100%', maxWidth: 500, margin: "10px auto", textAlign: "center"}}>
-            <Card sx={{ maxWidth: 345}}>
-                <CardActionArea>
-                    <CardMedia
-                    component="img"
-                    height="140"
-                    src={content.picture}
-                    alt="picture"
-                    />
-                    <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        {profile}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        {content}
-                    </Typography>
-                    </CardContent>
-                </CardActionArea>
-            </Card>
-        </Box>
-    </div>
-  );
+    useEffect(() => {
+      
+    
+      return () => {
+        
+      }
+    }, [globalEvents])
+    
+
+    // let pubkey = props.event.pubkey;
+    // let profile = await nip05.queryProfile(pubkey)
+
+        return (
+            <div>hi</div>
+            // globalEvents.map((event) => {
+            //     return (
+                    
+
+            //     )
+            
+        )
+
 }
--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -4,50 +4,67 @@ import { setEvents } from '../redux/nostr';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link as RouterLink, LinkProps as RouterLinkProps } from 'react-router-dom';
 import toastr from 'toastr';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Box, CardActionArea, Button } from '@mui/material';
 
 
-import { Box, Button } from '@mui/material';
-import { SimplePool } from 'nostr-tools';
+
+import { getEventHash, SimplePool } from 'nostr-tools';
+import { Container } from '@mui/system';
 
 function Feed(props) {
-    const [eventList, setEventList] = useState([]);
+    const [eventList, setNewEvents] = useState([]);
     const dispatch = useDispatch();
     const relays = useSelector(state => state.nostr.relayList);
+    
     const pool = new SimplePool()
 
     // newEvent.id = getEventHash(newEvent);
     // newEvent.sig = signEvent(newEvent, sk);
-
-    const getEvents = async () => {
-        let newEvents = await pool.list(relays, [{kinds: [1]}]);
-        let parsedEvents = [];
-        newEvents.forEach((event) => parsedEvents.push(JSON.stringify(event)))
-        setEventList(parsedEvents);
-        dispatch(setEvents(parsedEvents));
-        console.log(`Retrieved Events: ${eventList}`);
-    }
     
-    useEffect(()=> {
-        getEvents();
-    },[]);
+    const loadEvents = async () => {
+        try{
+            let globalEvents = await pool.list(relays, [{kinds: [1]}]);
+            setNewEvents(globalEvents)
+        } catch(error) {
+            console.log("event error: " + error)
+        }
+    }
 
-    if (eventList !== []){
-        return (
+    useEffect(() => {
+
+    }, [])
+
+    return (
+        <Container>
             <Box>
-                <Button href='/new-event'>
-                    New Event
+                <Button onClick={loadEvents}>
+                    Load Events
                 </Button>
-                <Box>
-                    {eventList.filter((value, index, self) => self.indexOf(value) === index).map((event) => (
-                        <Note key={event.id} event={event}/>
-                    ))}
-                </Box>
+                {eventList.map((event) => {
+                    return (
+                        <Container key={event.sig}>
+                            <Box sx={{ width: '100%', maxWidth: 500, margin: "10px auto", textAlign: "center"}}>
+                                <Card sx={{ maxWidth: 345}}>
+                                    <CardActionArea>
+                                        <CardContent>
+                                            <Typography color="text.secondary">
+                                                {event.content}
+                                            </Typography>
+                                        </CardContent>
+                                    </CardActionArea>
+                                </Card>
+                            </Box>
+                        </Container>
+                    )
+                })}
                 
             </Box>
-        )
-    } else {
-        return <div>Loading...</div>
-    }
-}
+        </Container>
+    )
 
+}
 export default Feed
