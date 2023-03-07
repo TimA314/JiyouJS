1adf945458fb3d6aa0da43d468c02bf454fa975f
TimA314 <ci0d7j9m@mailer.me>
2023-03-06 20:08:26 -0800

    set profiles

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,52 +1,61 @@
 import {React, useEffect, useState } from 'react';
 import Note from '../components/Note';
-import { setEvents } from '../redux/nostr';
+import { setEvents, setProfiles } from '../redux/nostr';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link as RouterLink, LinkProps as RouterLinkProps } from 'react-router-dom';
 import toastr from 'toastr';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { Box, CardActionArea, Button, Paper } from '@mui/material';
-import CachedIcon from '@mui/icons-material/Cached';
+import { Box, CardActionArea, Button, Paper, SvgIcon } from '@mui/material';
 import Avatar from '@mui/material/Avatar';
-import { createAvatar } from '@dicebear/core';
-import { identicon } from '@dicebear/collection';
-
-
-
 import { getEventHash, SimplePool } from 'nostr-tools';
 import { Container } from '@mui/system';
 
-function Feed(props) {
-    const [eventList, setNewEvents] = useState([]);
-    const [profileList, setProfileList] = useState([]);
 
-    const pool = new SimplePool()
-    const dispatch = useDispatch();
+
+
+function Feed(props) {
     const relays = useSelector(state => state.nostr.relayList);
-    const avatar = createAvatar(identicon, {
-        "seed": "Gracie"
-      });
-      const svg = avatar.toString();
-    // newEvent.id = getEventHash(newEvent);
-    // newEvent.sig = signEvent(newEvent, sk);
+    const events = useSelector(state => state.nostr.events);
+    const profileList = useSelector(state => state.nostr.profileList)
+    const dispatch = useDispatch();
+    
+    console.log("relays: " + relays)
     
     const loadEvents = async () => {
+        const pool = new SimplePool()
         try{
-            let globalEvents = await pool.list(relays, [{kinds: [0,1]}]);
-            let profiles = globalEvents.filter((event) => event.kind === 0);
-            let events = globalEvents.filter((event) => event.kind === 1);
 
-            setNewEvents(events)
-            setProfileList(profiles)
-            dispatch(setEvents(events))
-        } catch(error) {
-            console.log("event error: " + error)
+            let globalEvents = await pool.list(relays ?? ['wss://eden.nostr.land'], [{kinds: [0,1]}]);
+            console.log("globalEvents: " + globalEvents)
+            
+            
+            let events = globalEvents.filter((event) => event.kind === 1);
+            if (events && events.length >= 1){
+                console.log("events: " + events);
+                dispatch(setEvents(events))
+            }
+            
+            
+            let profiles = globalEvents.filter((event) => event.kind === 0);     
+            if (profiles && profiles.length >= 1){
+                console.log("profiles: " + profiles)
+                dispatch(setProfiles(profiles))
+            }
+            
+        } catch {
+            console.log("event error: ")
         }
+
     }
+    loadEvents()
+    setTimeout(() => {
+
+    }, 5000)
+    
+
     const getProfilePicture = (pubkey) => {
+        if (!pubkey) return <Avatar alt="profile" src="../assets/botttsNeutral-1678157518910.svg"></Avatar>
         let profile =  profileList.find(prof => {
             return prof.pubkey === pubkey
             });
@@ -55,26 +64,19 @@ function Feed(props) {
                 let parsedProfile = JSON.parse(profile.content);
                 console.log(parsedProfile.picture)
                 if (parsedProfile.picture !== undefined){
-                    return <Avatar alt="Remy Sharp" src={parsedProfile.picture.toString()} />
+                    return <Avatar alt="profile" src={parsedProfile.picture.toString()} />
                 }
             }
-        return 
+        return <Avatar alt="profile" src="../assets/botttsNeutral-1678157518910.svg"></Avatar>
     }
-
-    useEffect(() => {
-
-    }, [])
 
     return (
         <Container width="100%">
             <Container >
                 <Box >
-                    <Button onClick={loadEvents}>
-                        <CachedIcon />
-                    </Button>
                 </Box>
             </Container>
-            {eventList.map((event) => {
+            { events?.map((event) => {
                 return (
                     <Container key={event.sig}>
                         <Box sx={{ width: '100%', maxWidth: 500, margin: "10px auto", textAlign: "center"}}>
