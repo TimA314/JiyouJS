e90b9e3f89df8804f680cefbef91cfe7ae11fe65
TimA314 <ci0d7j9m@mailer.me>
2023-03-12 20:29:14 -0700

    display events in follower feed

--- src/pages/FollowerFeed.jsx
+++ src/pages/FollowerFeed.jsx
@@ -1,8 +1,7 @@
-import {React, useContext, useEffect, useRef, useState } from 'react';
+import {React, useEffect, useState } from 'react';
 import { getPublicKey, SimplePool } from 'nostr-tools';
 import { useNavigate } from 'react-router';
 import Note from '../components/Note';
-import { isValidKey } from '../NostrFunctions';
 import { sortEvents } from '../util';
 
 
@@ -20,33 +19,35 @@ function FollowerFeed(props) {
         const loadEvents = async () => {
             try{
                 let followerEvents = await pool.list(relays, [{kinds: [3], authors: [getPublicKey(privateKey)], limit: 1 }])
-                console.log(followerEvents[0])
 
                 if (!followerEvents[0] || !followerEvents[0].tags) return;
 
                 let followerArray = followerEvents[0].tags.filter((tag) => tag[0] === "p");
+                let followerPks = [];
+                for(let i=0; i<followerArray.length;i++){
+                    followerPks.push(followerArray[i][1]);
+                }
 
+                let poolOfEvents = await pool.list(relays, [{kinds: [1], authors: followerPks, limit: 100 }])
 
-                console.log(followerArray)
-                let poolOfEvents = await pool.list(relays, [{kinds: [1], authors: followerArray, limit: 100 }])
                 console.log("poolEvents" + JSON.stringify(poolOfEvents))
+
                 if(!poolOfEvents) {
-                    console.log("bad")
                     return;
                 }
 
-                let profileAddedEvents = [];
-
                 for(let i=0;i<poolOfEvents.length;i++) {
-                    profileAddedEvents.push(addProfileToEvent(poolOfEvents[i]));
-                }
+                    let prof = await pool.list(relays, [{kinds: [0], authors: [poolOfEvents[i].pubkey], limit: 1 }])
 
-                console.log("profileAddedEvents" + profileAddedEvents)
-                
-                setEvents((prevEvents) => {
-                    let newEvents = sortEvents(profileAddedEvents)
-                    return newEvents;
-                });
+                    if(prof){
+                        poolOfEvents[i].profile = prof;
+                    }
+
+                    setEvents((prevEvents) => {
+                        let newEvents = sortEvents([...prevEvents, poolOfEvents[i]])
+                        return newEvents;
+                    });
+                }
 
             } catch (error) {
                 console.error("event error: " + error)
@@ -58,18 +59,23 @@ function FollowerFeed(props) {
 
             if(prof){
                 addToProfileEvent.profile = prof;
+                return addProfileToEvent;
             }
         }
 
         loadEvents();
     }, [])
+
+    const unFollowEvent = (unfollowPk) => {
+        console.log("unfollow: " + unfollowPk)
+    }
     
     if (events && events.length > 0) {
         return (
             <>
                 {events.map(e => {
                     return (
-                        <Note key={e.sig + Math.random()} event={e} followEvent={null}/>
+                        <Note key={e.sig + Math.random()} event={e} followEvent={unFollowEvent}/>
                     )
                 })}
             </>
