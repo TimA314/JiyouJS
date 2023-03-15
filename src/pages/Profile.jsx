2248d42ac39ac6484491716b240642ffa815cef6
TimA314 <ci0d7j9m@mailer.me>
2023-03-14 20:31:32 -0700

    adjust styling

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,24 +1,27 @@
-import { AppBar, Avatar, Box, Button, Card, IconButton, InputAdornment, MenuItem, Paper, TextField, Toolbar, Typography } from '@mui/material'
+import { AppBar, Avatar, Box, Button, Card, IconButton, InputAdornment, MenuItem, Paper, TextField, Toolbar, Typography, useTheme } from '@mui/material'
 import { styled } from '@mui/system';
 import { getPublicKey, SimplePool } from 'nostr-tools';
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router';
+import useMediaQuery from '@mui/material/useMediaQuery';
 import SaveIcon from '@mui/icons-material/Save';
 
 
 
 export default function Profile(props) {
+const theme = useTheme();
+const smallScreen = useMediaQuery('(max-width:600px)');
 const [profile, setProfile] = useState(null);
 const [profileImageUrl, setProfileImageUrl] = useState("");
 const privateKey = window.localStorage.getItem("localPk");
 console.log("pk profile page : " + privateKey + "Session pk: " + privateKey);
 const navigate = useNavigate();
+console.log(smallScreen)
 
 // console.log(JSON.stringify(profile));
 // ----------------------------------------------------------------------
 
-
-const StyledContent = styled('div')(({ theme }) => ({
+const MediumToLargeScreenStyle = styled('div')(({ theme }) => ({
   maxWidth: 600,
   margin: 'auto',
   display: 'flex',
@@ -27,15 +30,34 @@ const StyledContent = styled('div')(({ theme }) => ({
   padding: theme.spacing(5, 0),
 }));
 
-const StyledAvatar = styled('div')(({ theme }) => ({
-  maxWidth: 600,
+const MediumToLargeAvatar = styled('div')(({ theme }) => ({
+  display: "flex",
+  margin: "auto",
+  flexDirection: "column",
+  justifyContent: "center"
+}));
+
+const SmallScreenStyle = styled('div')(({ theme }) => ({
+  maxWidth: 300,
   margin: 'auto',
   display: 'flex',
   justifyContent: 'center',
   flexDirection: 'column',
-  padding: theme.spacing(5, 12),
+  padding: theme.spacing(5, 0),
 }));
+
+const SmallScreenAvatar = styled('div')(({ theme }) => ({
+  maxWidth: 300,
+  margin: "auto",
+  display: 'flex',
+  justifyContent: 'center',
+  flexDirection:'column',
+}));
+
 // ----------------------------------------------------------------------
+
+const StyledContent = smallScreen ? SmallScreenStyle : MediumToLargeScreenStyle;
+const StyledToolbar = smallScreen ? SmallScreenAvatar : MediumToLargeAvatar;
 
 useEffect(() => {
   if (!privateKey || privateKey === "") navigate("/signin", {replace: true});
@@ -58,25 +80,25 @@ const handleLogout = (e) => {
 
 if(privateKey){
   return (
-    <Box>
+    <Box width="100%">
       <AppBar position="static" >
-        <Box sx={{}}>
+        <Box>
             <Button onClick={handleLogout}>Logout</Button>
         </Box>
         <Toolbar >
           <IconButton edge="start" color="inherit" aria-label="menu">
             <MenuItem />
           </IconButton>
-          <Typography variant="h6">
-            {profile && profile.content ? profile.content.name : getPublicKey(privateKey)}
-          </Typography>
-          <StyledAvatar>
-            <Avatar
-              src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973461_1280.png"
-              sx={{ width: 200, height: 200, zIndex: "right" }}
-              />
-          </StyledAvatar>
         </Toolbar>
+        <StyledToolbar>
+          <Avatar
+            src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973461_1280.png"
+            sx={{ width: 200, height: 200}}
+            />
+          <Typography variant="h6" alignSelf="center" margin="15px" display="flex">
+            {profile && profile.content ? profile.content.name : "Display Name"}
+          </Typography>
+        </StyledToolbar>
       </AppBar>
 
       <StyledContent>
